711f255d32f3aa08f6b0574148281ec65b068018
MC-Knight <78812884+MC-Knight@users.noreply.github.com>
2022-05-19 19:57:31 +0200

    Create cSharpHelloWorld.cs
    cSharp output with Console.Write or WriteLine

---
+++ cSharpHelloWorld.cs
@@ -0,0 +1,16 @@
+Console.WriteLine("Hello World");
+
+
+string name = "Joseph");
+
+//output with WriteLine which outputs with new line like theone \n in other many languages
+Console.WriteLine($"Hello {name} !);
+Console.WriteLine("Hello" +name);
+
+//output with Write only which write without breaking a line
+Console.Write("Hello there");
+Console.Write("How are you doing");
+
+//it comes [like Hello there How are you doing] instead of two lines
+
+
